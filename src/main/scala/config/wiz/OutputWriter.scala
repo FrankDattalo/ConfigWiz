f1efc7bd3e939dd16b024915d351207269b6f0bc
Frank Dattalo <Frank.Dattalo@alliancedata.com>
2017-05-26 09:06:52 -0400

    Refactor

--- src/main/scala/config/wiz/OutputWriter.scala
+++ src/main/scala/config/wiz/OutputWriter.scala
@@ -10,32 +10,35 @@ object OutputWriter {
   def write(outputLocation: String, tasks: ArrayBuffer[Dependency]): Unit = {
     val file = new File(outputLocation)
 
-    if(file.exists) {
-      Logger.log(s"Removing contents of directory ${file.getAbsolutePath}")
-      FileUtils.cleanDirectory(file)
-
-    } else if (!file.mkdir) {
-      throw new IOException(s"Could not create directory: ${file.getAbsolutePath}")
-    }
+    cleanDirectory(file)
 
     tasks.remove(tasks.length - 1)
 
     var i = 1
+    
     tasks foreach { task =>
       val from = new File(task.fileFetcher.baseUrl)
       val to = new File(outputLocation + s"[$i]-${task.url}")
 
-      Logger.log(s"Creating directory ${to.getAbsolutePath}")
-
-      if(!to.mkdir) {
-        throw new IOException(s"Could not create directory ${to.getAbsolutePath}")
-      }
+      cleanDirectory(to)
 
       Logger.log(s"Copying ${from.getAbsolutePath} to ${to.getAbsolutePath}")
 
       FileUtils.copyDirectory(from, to)
 
       i += 1
+    }
+  }
+  
+  def cleanDirectory(f: File) {
+     if(f.exists) {
+      Logger.log(s"Removing contents of directory ${f.getAbsolutePath}")
+      FileUtils.cleanDirectory(f)
+    } else {
+      Logger.log(s"Creating directory ${f.getAbsolutePath}")       
+      if (!file.mkdir) {
+        throw new IOException(s"Could not create directory: ${file.getAbsolutePath}")
+      }
     }
   }
 }
